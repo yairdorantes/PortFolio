e3858a1dcb2b3be112f4748f15df89850fbb396d
Yair Dorantes <yairmasterlol@gmail.com>
2022-06-20 18:28:17 -0500

    start portfolio

--- javascript.js
+++ javascript.js
@@ -23,34 +23,13 @@ terminal = [
 const d = document;
 
 const pageLoading = d.querySelector(".welcome");
-var width = 0;
-function appear() {
-  const barra = d.querySelector(".progress");
 
-  pageLoading.classList.add("appear");
-  const intervalo = setInterval(() => {
-    width += 1;
-    if (width > 200) {
-      clearInterval(intervalo);
-      //pageLoading.classList.remove("appear");
-      pageLoading.classList.add("disappear");
-      setTimeout(() => {
-        width = 0;
-        pageLoading.classList.remove("disappear");
-        pageLoading.classList.remove("appear");
-      }, 800);
-    }
-    barra.style.width = `${width}px`;
-  }, 10);
-}
-
-d.addEventListener("DOMContentLoaded", (e) => {
-  appear();
-});
+const showTerminal = d.querySelector(".disappear-my-data");
 
 const phraseDeveloper = d.getElementById("be");
 const updates = d.getElementById("info-update");
-const clearing = d.getElementById("clear");
+const clearing = d.getElementById("clean");
+const clearingTxt = d.getElementById("clear");
 let str = "sudo apt-get update";
 
 let index = 0;
@@ -60,29 +39,64 @@ let clear = "clear";
 
 const brHtml = d.createElement("br");
 
-let intervalId = setInterval(function () {
-  phraseDeveloper.textContent += str[index];
+var width = 0;
+function appear() {
+  const barra = d.querySelector(".progress");
 
-  index++;
-  if (index === str.length) {
-    clearInterval(intervalId);
-    /***aparecen actualizaciones */
-    let intervalUpdate = setInterval(function () {
-      updates.textContent += terminal[indexUpdate] + brHtml;
-      indexUpdate++;
-      if (indexUpdate === terminal.length) {
-        clearInterval(intervalUpdate);
-        /*****fin actualizaciones******/
-        /****write clear */
-        let intervalClear = setInterval(function () {
-          clearing.textContent += clear[indexClear];
-          indexClear++;
-          if (indexClear === clear.length) {
-            clearInterval(intervalClear);
-          }
-        }, 300);
-        /****ends-write clear */
-      }
-    }, 200);
-  }
-}, 200);
+  pageLoading.classList.add("appear");
+  const intervalo = setInterval(() => {
+    width += 1;
+    if (width > 200) {
+      clearInterval(intervalo);
+      showTerminal.classList.remove("disappear-my-data");
+      showTerminal.classList.add("appear-my-data");
+      //pageLoading.classList.remove("appear");
+      pageLoading.classList.add("disappear");
+      setTimeout(() => {
+        width = 0;
+        pageLoading.classList.remove("disappear");
+        pageLoading.classList.remove("appear");
+      }, 800);
+
+      setTimeout(() => {
+        showMyTerminal();
+      }, 700);
+    }
+    barra.style.width = `${width}px`;
+  }, 10);
+}
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  appear();
+});
+
+function showMyTerminal() {
+  let intervalId = setInterval(function () {
+    phraseDeveloper.textContent += str[index];
+
+    index++;
+    if (index === str.length) {
+      clearInterval(intervalId);
+      /***aparecen actualizaciones */
+      let intervalUpdate = setInterval(function () {
+        updates.textContent += terminal[indexUpdate] + brHtml;
+        indexUpdate++;
+        if (indexUpdate === terminal.length) {
+          clearInterval(intervalUpdate);
+          /*****fin actualizaciones******/
+          /****write clear */
+          clearing.textContent += ">_:";
+
+          let intervalClear = setInterval(function () {
+            clearingTxt.textContent += clear[indexClear];
+            indexClear++;
+            if (indexClear === clear.length) {
+              clearInterval(intervalClear);
+            }
+          }, 300);
+          /****ends-write clear */
+        }
+      }, 200);
+    }
+  }, 200);
+}
